7ad745164147b1b240857ca22cdf511f08aca368
Jay <jaychakra.jc@gmail.com>
2025-09-28 11:02:33 +0530

    More on Async Programming

--- S3-AsyncProgramming/playground.js
+++ S3-AsyncProgramming/playground.js
@@ -1,38 +1,55 @@
-const asyncFunction1 = (cb) => {
+// Async Await
+
+
+
+function asyncFunction2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Async funtion 2 called");            
+            reject(2)    
+        }, 2000)
+   })
+}
+
+
+const asyncFunction3 = () => {
+   return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Async funtion 3 called");            
+            resolve(3)    
+        }, 2000)
+   })
+}
+
+
+const asyncFunction1 = () => {
+   return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Async funtion 1 called");            
+            resolve(1)    
+        }, 2000)
+   })
+}
+
+const asyncFunction4 = () => {
     setTimeout(() => {
-        console.log("Async Function 1");            
-        cb();
-    }, 1000);
-};
-
-const asyncFunction2 = (cb) => {
-    setTimeout(() => {
-        console.log("Async Function 2");
-        cb();
-    }, 1000);
-};
-
-const asyncFunction3 = (cb) => {
-    setTimeout(() => {
-        console.log("Async Function 3");
-        cb();
-    }, 1000);
-};
-
-const callback3 = () => {
-    console.log("All Async Functions Completed");
+        console.log("Async funtion 1 called");            
+    }, 2000)
+ 
 }
 
-const callback2 = () => {
-    asyncFunction3(callback3);
+const sum = async (a, b) => a + b;
+
+const main = async () => {
+    const r1 = await asyncFunction1();
+    const r2 = await asyncFunction2();
+    const r3 = await asyncFunction3();
+    console.log(r1,r2,r3)
+    
+    // const res = await sum(4, 5);
+    
+    // console.log(res);
 }
 
-const callback1 = () => {
-    asyncFunction2(callback2);
-}
-
-const main = () => {
-    asyncFunction1(callback1);
-}
 
 main();
